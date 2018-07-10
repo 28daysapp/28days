2d1fd04ca97882f0da5d8e9a2e159e7f84a3d685
susuzy <csg27@naver.com>
2018-07-10 15:50:50 +0900

    mypage mypost

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,6 +21,7 @@ import { CommunityProvider } from '../providers/community/community';
 import { CommunitycommentProvider } from '../providers/communitycomment/communitycomment';
 import { PostProvider } from '../providers/post/post';
 import { CommunityfixPage } from '../pages/communityfix/communityfix'
+import { MyProvider } from '../providers/my/my'
 
 // import { IamportService } from 'iamport-ionic-kcp';
 import { InAppBrowser } from '@ionic-native/in-app-browser';
@@ -60,6 +61,7 @@ import { Geolocation } from '@ionic-native/geolocation';
     CommunityProvider,
     CommunitycommentProvider,
     PostProvider,
+    MyProvider,
     CommunityfixPage,
     // IamportService,
     InAppBrowser,
