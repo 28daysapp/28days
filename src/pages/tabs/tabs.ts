7dafef965cf88e17302a49dfff042d069f4f93b7
Minseung <qwert8250@gmail.com>
2018-09-03 18:09:47 +0900

    etc

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -28,39 +28,8 @@ export class TabsPage {
   tab3Title = "온라인상담";
   tab4Title = "서포터/상담사";
   tab5Title = "커뮤니티";
-  lastBack;
-  allowClose;
+  backBtn;
   constructor(public navCtrl: NavController, private app: App, private toastCtrl: ToastController, public platform: Platform, ) {
-    platform.ready().then(() => {
-      platform.registerBackButtonAction(() => {
-        const overlay = this.app._appRoot._overlayPortal.getActive();
-        const nav = this.app.getActiveNav();
-        const closeDelay = 2000;
-        const spamDelay = 500;
 
-        if (overlay && overlay.dismiss) {
-          overlay.dismiss();
-        }
-        else if (nav.canGoBack()) {
-          nav.pop();
-        }
-        else if (Date.now() - this.lastBack > spamDelay && !this.allowClose) {
-          this.allowClose = true;
-          let toast = this.toastCtrl.create({
-            message: "뒤로 버튼을 한번 더 누르시면 종료합니다.",
-            duration: closeDelay,
-            dismissOnPageChange: true,
-          });
-          toast.onDidDismiss(() => {
-            this.allowClose = false;
-          });
-          toast.present();
-        } else if (Date.now() - this.lastBack < closeDelay && this.allowClose) {
-          platform.exitApp();
-        }
-
-        this.lastBack = Date.now();
-      })
-    });
   }
 }
