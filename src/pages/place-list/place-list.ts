5d560203925e6444d8cfe281fdedcfb5c9efc807
Yung Ju Rick Kim <yungjurick@gmail.com>
2018-12-10 17:56:51 +0900

    Add Functionalities to Place Provider for Premium Places DB

--- src/pages/place-list/place-list.ts
+++ src/pages/place-list/place-list.ts
@@ -1,39 +1,44 @@
-import { Component, ViewChild, ElementRef } from '@angular/core';
-import { NavController, NavParams, IonicPage, AlertController, LoadingController } from 'ionic-angular';
-import firebase from 'firebase';
-
+import { Component, ViewChild, ElementRef } from "@angular/core";
+import {
+  NavController,
+  NavParams,
+  IonicPage,
+  AlertController,
+  LoadingController
+} from "ionic-angular";
+import { PlaceProvider } from "../../providers/place/place";
+import firebase from "firebase";
 
 declare let google;
 
 @IonicPage()
 @Component({
-  selector: 'page-place-list',
-  templateUrl: 'place-list.html',
+  selector: "page-place-list",
+  templateUrl: "place-list.html"
 })
 export class PlaceListPage {
-
   @ViewChild("map") mapElement: ElementRef;
 
   map: any;
   latLng: any;
 
-  placeType: String = "center"
+  placeType: String = "center";
   searchQuery: string;
+  premiumPlaces: any = [];
   places: any = [];
 
   adExists: boolean = true;
 
   constructor(
-    public navCtrl: NavController, 
-    public navParams: NavParams, 
+    public navCtrl: NavController,
+    public navParams: NavParams,
     public alertCtrl: AlertController,
-    public loadingCtrl: LoadingController
-    ) {
-    
-  }
+    public loadingCtrl: LoadingController,
+    public placeProvider: PlaceProvider
+  ) {}
 
   ionViewWillEnter() {
-    console.log('ionViewDidLoad PlaceListPage');
+    console.log("ionViewDidLoad PlaceListPage");
 
     this.loadMap();
   }
@@ -58,14 +63,14 @@ export class PlaceListPage {
     // });
 
     // If Google Api current location is disabled, default location is Seoul City Hall
-    this.latLng = new google.maps.LatLng(37.532600, 127.024612)
+    this.latLng = new google.maps.LatLng(37.5326, 127.024612);
 
     const mapOptions = {
       center: this.latLng,
       zoom: 14,
       mapTypeId: google.maps.MapTypeId.ROADMAP,
       streetViewControl: false
-    }
+    };
     this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
 
     this.searchByText("강남");
@@ -74,20 +79,30 @@ export class PlaceListPage {
   }
 
   searchByText(userInput) {
-    this.presentLoading()
+    this.presentLoading();
 
     if (!userInput) {
-      userInput = "강남"
+      userInput = "강남";
     }
-    if (this.placeType === "psychiatric" ? this.searchQuery = "정신과" : this.searchQuery = "심리상담센터") {
 
-      this.latLng = new google.maps.LatLng(37.532600, 127.024612)
+    // --------- Getting Premium Place DB ------------
+
+    this.premiumPlaces = this.getPlaceList(this.placeType);
+
+    // -----------------------------------------------
+
+    if (
+      this.placeType === "hospital"
+        ? (this.searchQuery = "정신과")
+        : (this.searchQuery = "심리상담센터")
+    ) {
+      this.latLng = new google.maps.LatLng(37.5326, 127.024612);
 
       let request = {
         location: this.latLng,
-        radius: '500',
+        radius: "500",
         query: userInput + " " + this.searchQuery,
-        language: 'ko',
+        language: "ko",
         type: ["hospital", "health", "doctor"]
       };
 
@@ -95,66 +110,69 @@ export class PlaceListPage {
       service.textSearch(request, (results, status) => {
         if (status == google.maps.places.PlacesServiceStatus.OK) {
           for (let i = 0; i < results.length; i++) {
-
             const randomNumber = Math.floor(Math.random() * 4) + 1;
             this.places = results;
             this.places[i].reviewCount = 0;
             this.places[i].ratings = 0;
-            this.places[i].image = "assets/imgs/hospital-default" + randomNumber + ".svg";
+            this.places[i].image =
+              "assets/imgs/hospital-default" + randomNumber + ".svg";
             this.countReviews(this.places[i].place_id, i);
-
           }
         } else {
           console.log("Status error: " + status);
         }
-      })
+      });
     }
   }
 
   countReviews(placeId, i) {
-    firebase.database().ref('/placeInfo/' + placeId).once('value').then((snapshot) => {
-      if (snapshot.val() === null) {
-        return
-      }
-      this.places[i].reviewCount = snapshot.val().reviewCount;
-      this.places[i].ratings = snapshot.val().ratings;
-    })
+    firebase
+      .database()
+      .ref("/placeInfo/" + placeId)
+      .once("value")
+      .then(snapshot => {
+        if (snapshot.val() === null) {
+          return;
+        }
+        this.places[i].reviewCount = snapshot.val().reviewCount;
+        this.places[i].ratings = snapshot.val().ratings;
+      });
   }
 
   placeDetail(place) {
-    this.navCtrl.push('PlaceDetailPage', { place: place });
+    this.navCtrl.push("PlaceDetailPage", { place: place });
   }
 
   showExplanation() {
     let alert = this.alertCtrl.create({
-      title: '정신병원과 상담센터?',
+      title: "정신병원과 상담센터?",
       message: `
       <p><strong>정신과</strong>: 약물 치료가 병행될 수 있고 보험 처리가 되어서 상담센터보다 조금 더 저렴한 편이야.</p>
 
       <p><strong>상담센터</strong>: 상담자와의 대화를 통해 자신을 좀 더 깊게 이해하고 건강한 마음을 회복할 수 있도록 돕는 곳이야.</p>
       `,
-      buttons: ['알겠어!']
+      buttons: ["알겠어!"]
     });
     alert.present();
   }
 
   sort() {
     let alert = this.alertCtrl.create({
-      title: '정렬 순서',
-      buttons: [{ text: '후기 많은 순' }, { text: '별점 높은 순' }]
+      title: "정렬 순서",
+      buttons: [{ text: "후기 많은 순" }, { text: "별점 높은 순" }]
     });
     alert.present();
   }
 
   notReady() {
     let alert = this.alertCtrl.create({
-      title: '알림',
-      message: '아직 준비중인 서비스입니다.',
+      title: "알림",
+      message: "아직 준비중인 서비스입니다.",
       buttons: [
         {
-          text: '확인',
-          role: 'cancel'
-        },
+          text: "확인",
+          role: "cancel"
+        }
       ]
     });
     alert.present();
@@ -162,10 +180,10 @@ export class PlaceListPage {
 
   presentLoading() {
     const loading = this.loadingCtrl.create();
-    loading.present()
+    loading.present();
     setTimeout(() => {
       loading.dismiss();
-    }, 300)
+    }, 300);
   }
 
   doRefresh(refresher) {
@@ -174,4 +192,9 @@ export class PlaceListPage {
     }, 2000);
   }
 
+  /* ------ Functions for Calling Premium Place DB ------ */
+
+  async getPlaceList(reference) {
+    return await this.placeProvider.readPlaceList(reference);
+  }
 }
--- src/providers/place/place.ts
+++ src/providers/place/place.ts
@@ -1,11 +1,31 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import firebase from "firebase";
 
 @Injectable()
 export class PlaceProvider {
+  firestore = firebase.storage();
 
   constructor() {
-    console.log('Hello PlaceProvider Provider');
+    console.log("Hello PlaceProvider Provider");
   }
 
+  // <Function to Read Place List>
+  // Reference: String, Accepting Values: 'hospital', 'center'
+  readPlaceList(reference: String) {
+    return new Promise(resolve => {
+      firebase
+        .database()
+        .ref(`${reference}Information`)
+        .once("value")
+        .then(snapshot => {
+          const places = [];
+          snapshot.forEach(childSnapshot => {
+            const place = childSnapshot.val();
+            places.push(place);
+          });
+          console.log(places);
+          resolve(places);
+        });
+    });
+  }
 }
-
