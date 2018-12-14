8b854e30d8783121dd13f6881f7881943b645e83
Byungho Lee <andyleeboo92@gmail.com>
2018-12-14 18:05:43 +0900

    Remove unnecessary images and icons

--- src/pages/myprofile/myprofile.ts
+++ src/pages/myprofile/myprofile.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, AlertController, LoadingController } from 'ionic-angular';
-import firebase from 'firebase';
-import { AuthProvider } from '../../providers/auth/auth';
-import { UserProvider } from '../../providers/user/user';
-import { Storage } from '@ionic/storage';
-import { NavParams, ModalController } from 'ionic-angular';
+import { Component } from "@angular/core";
+import {
+  IonicPage,
+  NavController,
+  AlertController,
+  LoadingController
+} from "ionic-angular";
+import firebase from "firebase";
+import { AuthProvider } from "../../providers/auth/auth";
+import { UserProvider } from "../../providers/user/user";
+import { Storage } from "@ionic/storage";
+import { NavParams, ModalController } from "ionic-angular";
 
 /**
  * Generated class for the MyprofilePage page.
@@ -15,8 +20,8 @@ import { NavParams, ModalController } from 'ionic-angular';
 
 @IonicPage()
 @Component({
-  selector: 'page-myprofile',
-  templateUrl: 'myprofile.html',
+  selector: "page-myprofile",
+  templateUrl: "myprofile.html"
 })
 export class MyprofilePage {
   loading;
@@ -28,42 +33,50 @@ export class MyprofilePage {
   origGreeting;
   showmodal = false;
 
-  constructor(public navCtrl: NavController, public alertCtrl: AlertController, public auth: AuthProvider, public userProvider: UserProvider,
-    public storage: Storage, public loadingCtrl: LoadingController, public params: NavParams, public modalCtrl: ModalController) {
+  constructor(
+    public navCtrl: NavController,
+    public alertCtrl: AlertController,
+    public auth: AuthProvider,
+    public userProvider: UserProvider,
+    public storage: Storage,
+    public loadingCtrl: LoadingController,
+    public params: NavParams,
+    public modalCtrl: ModalController
+  ) {
     // Receive message from push notifications
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad MyprofilePage');
+    console.log("ionViewDidLoad MyprofilePage");
     this.loading = this.loadingCtrl.create();
     this.loading.present();
 
     // set defualt user photo
-    this.photoURL = 'assets/profile0.png';
+    this.photoURL = "assets/imgs/post5.jpg";
 
     // check if user already logged-in
     this.user = firebase.auth().currentUser;
-      // user already logged-in
-      this.username = this.user.displayName;
-      this.photoURL = this.user.photoURL;
-      
-      this.userProvider.getUserprofile(this.user.uid).then((userprofile) => {
-        this.userprofile = userprofile;
-        this.greeting = this.userprofile.greeting;
-        this.loading.dismiss();
-      })
+    // user already logged-in
+    this.username = this.user.displayName;
+    this.photoURL = this.user.photoURL;
+
+    this.userProvider.getUserprofile(this.user.uid).then(userprofile => {
+      this.userprofile = userprofile;
+      this.greeting = this.userprofile.greeting;
+      this.loading.dismiss();
+    });
   }
 
   changecharacter() {
-      this.navCtrl.push('CharacterPage');
+    this.navCtrl.push("CharacterPage");
   }
 
   changegreeting() {
-      // this.navCtrl.push('SupporterPage');
-      setTimeout(() => {
-        this.origGreeting = this.greeting;
-        this.showmodal = true;
-      }, 500);
+    // this.navCtrl.push('SupporterPage');
+    setTimeout(() => {
+      this.origGreeting = this.greeting;
+      this.showmodal = true;
+    }, 500);
   }
   modalhandler(modal) {
     this.greeting = modal.value.greeting;
@@ -76,7 +89,7 @@ export class MyprofilePage {
     this.greeting = this.origGreeting;
   }
 
-  pwdcheck(){
-    this.navCtrl.push('PwdcheckPage');
+  pwdcheck() {
+    this.navCtrl.push("PwdcheckPage");
   }
 }
